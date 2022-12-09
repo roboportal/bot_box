49711f7ad8bc4ce80e994b1557f1cfb17185b908
Denys Malykhin <malykhin@ukr.net>
2022-12-08 18:28:11 -0800

    catch error

--- pkg/botcom/botcom.go
+++ pkg/botcom/botcom.go
@@ -68,8 +68,14 @@ func Init(p InitParams) {
 
 			peerConnection, err = p.Api.NewPeerConnection(config)
 
-			defer peerConnection.Close()
-			defer haltControls(p.BotCommandsWriteChan, p.Id)
+			defer func() {
+				if r := recover(); r != nil {
+					fmt.Println("pion panic:", r)
+				}
+
+				peerConnection.Close()
+				haltControls(p.BotCommandsWriteChan, p.Id)
+			}()
 
 			if err != nil {
 				log.Println("Create peerConnection error", err)
