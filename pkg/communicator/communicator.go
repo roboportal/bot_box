9e939fe28b23d0a8591a0ddeffab54898894d18c
Denys Malykhin <malykhin@ukr.net>
2023-03-24 17:34:20 -0700

    remove recursion in communicator

--- pkg/communicator/communicator.go
+++ pkg/communicator/communicator.go
@@ -6,6 +6,8 @@ import (
 	"context"
 	"time"
 	"github.com/gorilla/websocket"
+
+	"github.com/roboportal/bot_box/pkg/utils"
 )
 
 const (
@@ -26,38 +28,115 @@ type InitParams struct {
 	ConStatChan         chan string
 }
 
-func nicelyClose(ch chan bool) {
-	select {
-	case <-ch:
-		return
-	default:
-	}
-	close(ch)
-}
 
 // Init setups ws connection
 func Init(p InitParams) {
-	ctx := context.Background()
-	status := disconnected
-	p.ConStatChan <- status
-
-	headers := http.Header{"x-public-key": {p.PublicKey}}
-
-	tickerPing := time.NewTicker(time.Duration(p.PingIntervalSec) * time.Second)
-	tickerPong := time.NewTicker(time.Duration(p.PingIntervalSec * 3) * time.Second)
-
-	status = connecting
-	p.ConStatChan <- status
-
-	log.Println("Connecting to the platform.")
-
-	dialer := websocket.Dialer{}
-	conn, _, err := dialer.DialContext(ctx, p.PlatformUri, headers)
-
-	done := make(chan bool)
-
-	defer (func() {
+	for {
+		ctx := context.Background()
 		status := disconnected
+		p.ConStatChan <- status
+
+		headers := http.Header{"x-public-key": {p.PublicKey}}
+
+		tickerPing := time.NewTicker(time.Duration(p.PingIntervalSec) * time.Second)
+		tickerPong := time.NewTicker(time.Duration(p.PingIntervalSec * 3) * time.Second)
+
+		status = connecting
+		p.ConStatChan <- status
+
+		log.Println("Connecting to the platform.")
+
+		dialer := websocket.Dialer{}
+		conn, _, err := dialer.DialContext(ctx, p.PlatformUri, headers)
+
+		done := make(chan struct{})
+
+
+		if err != nil {
+			log.Println("Connection failed error:", err)
+			break
+		}
+
+		conn.SetPongHandler(func(d string) error {
+			tickerPong.Reset(time.Duration(p.PingIntervalSec * 3) * time.Second)
+
+			return nil
+		})
+
+
+		status = connected
+		p.ConStatChan <- status
+
+		log.Println("Connected to the platform.")
+		
+		go (func() {
+			for {
+				select {
+				case <-done:
+					return
+
+				default:
+					if status != connected {
+						continue
+					}
+
+					_, message, err := conn.ReadMessage()
+
+					if err != nil {
+						log.Println("WS receiving error", err)
+						utils.NicelyClose(done)
+						return
+					}
+
+					if message != nil {
+						p.ReceiveChan <- string(message)
+					}
+				}
+			}
+		})()
+
+		tickerPing.Reset(time.Duration(p.PingIntervalSec) * time.Second)
+		tickerPong.Reset(time.Duration(p.PingIntervalSec * 3) * time.Second)
+
+		for {
+			select {
+			case <-done:
+				break
+
+			case <-tickerPong.C:
+				utils.NicelyClose(done)
+				break
+
+			case <-tickerPing.C:
+				if status != connected {
+					continue
+				}
+
+				err := conn.WriteControl(websocket.PingMessage, []byte(p.PublicKey), time.Now().Add(time.Duration(p.SendTimeoutSec)*time.Second))
+
+				if err != nil {
+					utils.NicelyClose(done)
+					break
+				}
+
+			case msg := <-p.SendChan:
+				if status != connected {
+					panic("Trying to send data thru closed socket")
+				}
+
+				conn.SetWriteDeadline(time.Now().Add(time.Duration(p.SendTimeoutSec) * time.Second))
+
+				err := conn.WriteMessage(websocket.TextMessage, []byte(msg))
+
+				if err != nil {
+					log.Println("Send WS data error:", err)
+					utils.NicelyClose(done)
+					break
+				}
+			}
+		}
+
+		status = disconnected
 		p.ConStatChan <- status
 
 		tickerPing.Stop()
@@ -68,90 +147,5 @@ func Init(p InitParams) {
 		}
 
 		time.Sleep(time.Duration(p.ReconnectTimeoutSec) * time.Second)
-
-		go Init(p)
-	})()
-
-	if err != nil {
-		log.Println("Connection failed error:", err)
-		return
-	}
-
-	conn.SetPongHandler(func(d string) error {
-		tickerPong.Reset(time.Duration(p.PingIntervalSec * 3) * time.Second)
-
-		return nil
-	})
-
-
-	status = connected
-	p.ConStatChan <- status
-
-	log.Println("Connected to the platform.")
-	
-	go (func() {
-		for {
-			select {
-			case <-done:
-				return
-
-			default:
-				if status != connected {
-					continue
-				}
-
-				_, message, err := conn.ReadMessage()
-
-				if err != nil {
-					log.Println("WS receiving error", err)
-					nicelyClose(done)
-					return
-				}
-
-				if message != nil {
-					p.ReceiveChan <- string(message)
-				}
-			}
-		}
-	})()
-
-	tickerPing.Reset(time.Duration(p.PingIntervalSec) * time.Second)
-	tickerPong.Reset(time.Duration(p.PingIntervalSec * 3) * time.Second)
-
-	for {
-		select {
-		case <-done:
-			return
-		case <-tickerPong.C:
-			nicelyClose(done)
-			return
-
-		case <-tickerPing.C:
-			if status != connected {
-				continue
-			}
-
-			err := conn.WriteControl(websocket.PingMessage, []byte(p.PublicKey), time.Now().Add(time.Duration(p.SendTimeoutSec)*time.Second))
-
-			if err != nil {
-				nicelyClose(done)
-				return
-			}
-
-		case msg := <-p.SendChan:
-			if status != connected {
-				panic("Trying to send data thru closed socket")
-			}
-
-			conn.SetWriteDeadline(time.Now().Add(time.Duration(p.SendTimeoutSec) * time.Second))
-
-			err := conn.WriteMessage(websocket.TextMessage, []byte(msg))
-
-			if err != nil {
-				log.Println("Send WS data error:", err)
-				nicelyClose(done)
-				return
-			}
-		}
 	}
 }
--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -3,3 +3,12 @@ package utils
 func TriggerChannel(c chan struct{}) {
 	c <- struct{}{}
 }
+
+func NicelyClose(ch chan struct{}) {
+	select {
+	case <-ch:
+		return
+	default:
+	}
+	close(ch)
+}
