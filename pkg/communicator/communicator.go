9fc9297ab54adf88bb26f382b19a8677c8acfac8
Denys Malykhin <malykhin@ukr.net>
2023-11-13 20:11:48 -0800

    update logging

--- pkg/communicator/communicator.go
+++ pkg/communicator/communicator.go
@@ -1,11 +1,12 @@
 package communicator
 
 import (
-	"sync"
+	"context"
 	"log"
 	"net/http"
-	"context"
+	"sync"
 	"time"
+
 	"github.com/gorilla/websocket"
 
 	"github.com/roboportal/bot_box/pkg/utils"
@@ -29,7 +30,6 @@ type InitParams struct {
 	ConStatChan         chan string
 }
 
-
 // Init setups ws connection
 func Init(p InitParams) {
 	for {
@@ -41,7 +41,7 @@ func Init(p InitParams) {
 		headers := http.Header{"x-public-key": {p.PublicKey}}
 
 		tickerPing := time.NewTicker(time.Duration(p.PingIntervalSec) * time.Second)
-		tickerPong := time.NewTicker(time.Duration(p.PingIntervalSec * 3) * time.Second)
+		tickerPong := time.NewTicker(time.Duration(p.PingIntervalSec*3) * time.Second)
 
 		status = connecting
 		p.ConStatChan <- status
@@ -53,29 +53,27 @@ func Init(p InitParams) {
 
 		done := make(chan struct{})
 
-
 		if err != nil {
 			log.Println("Connection failed error:", err)
 
 			log.Println("Sleeping before reconnecting.")
 
 			time.Sleep(time.Duration(p.ReconnectTimeoutSec) * time.Second)
-			
+
 			continue
 		}
 
 		conn.SetPongHandler(func(d string) error {
-			tickerPong.Reset(time.Duration(p.PingIntervalSec * 3) * time.Second)
+			tickerPong.Reset(time.Duration(p.PingIntervalSec*3) * time.Second)
 
 			return nil
 		})
-
 
 		status = connected
 		p.ConStatChan <- status
 
 		log.Println("Connected to the platform.")
-		
+
 		go (func() {
 			wg.Add(1)
 
@@ -107,15 +105,17 @@ func Init(p InitParams) {
 		})()
 
 		tickerPing.Reset(time.Duration(p.PingIntervalSec) * time.Second)
-		tickerPong.Reset(time.Duration(p.PingIntervalSec * 3) * time.Second)
+		tickerPong.Reset(time.Duration(p.PingIntervalSec*3) * time.Second)
 
 		for loop := true; loop; {
 			select {
 			case <-done:
+				log.Println("Exiting communicator loop")
 				loop = false
 				break
 
 			case <-tickerPong.C:
+				log.Println("Pong timeout")
 				utils.NicelyClose(done)
 				loop = false
 				break
@@ -128,6 +128,7 @@ func Init(p InitParams) {
 				err := conn.WriteControl(websocket.PingMessage, []byte(p.PublicKey), time.Now().Add(time.Duration(p.SendTimeoutSec)*time.Second))
 
 				if err != nil {
+					log.Println("WriteControl error:", err)
 					utils.NicelyClose(done)
 					loop = false
 					break
